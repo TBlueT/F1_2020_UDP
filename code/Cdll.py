ca0a3c3cefd3ebfb24592c88d64ec75659f80b72
TBlueT <81542666+TBlueT@users.noreply.github.com>
2021-12-12 14:50:37 +0900

    Add files via upload

--- code/Cdll.py
+++ code/Cdll.py
@@ -4,7 +4,7 @@ import os
 
 class DLL:
     def __init__(self):
-        self.lodDll = windll.LoadLibrary(os.path.dirname(os.path.realpath(__file__)) + "\\Maths.dll")
+        self.lodDll = windll.LoadLibrary(os.path.dirname(os.path.abspath(__file__)) + "\\Maths.dll")
 
     def Function(self, name, input, output):
         temporary = self.lodDll[name]
